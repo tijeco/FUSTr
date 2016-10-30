903173fa2eda4412e0a389fde60ba9055931956a
jffcole7 <jffcole7@gmail.com>
2016-10-29 22:55:03 -0400

    new change

--- KeepLongestIsoformID.R
+++ KeepLongestIsoformID.R
@@ -3,4 +3,4 @@ transcripts <- read.table(args[1])
 
 aa <- transcripts[order(transcripts$V1, -abs(transcripts$V3) ), ]
 
-write.table( aa[ !duplicated(aa$V1), ] ,file="args[2]",row.names=F,col.names=F,quote=F)
+write.table( aa[ !duplicated(aa$V1), ] ,file=args[2],row.names=F,col.names=F,quote=F)
