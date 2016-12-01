3ca26095f28d243addcabfd28a73b8957f1edd03
Jeff <coleti16@students.ecu.edu>
2016-12-01 11:06:45 -0500

    new

--- nuc2aa.py
+++ nuc2aa.py
@@ -1,73 +1,60 @@
-nuc ="ATGAACTTCTCTACAATCATCATACTACTGATTATTGGCATTTCTTACGCTACTGGACTGTCAACTAATCAAGGATCAACCAGAAGTAAAAAAGGAGCTCTTATGGAAATTGAAAGAAGGTGTATTGGAAAAGAGCAGGAATGCACTGATAACAAAGGAGGGTGCTGTGGTGACATGAAATGCTTATGTTACAAGAATGTCATGGATGAAAACGAGAAAGGATGCTGGTGTGGATCTGGAACCTACTACTTCATTGAAAAACCTTAA"
-print len(nuc)/3
-position = 0
+cds="ATGTTTGCTAAGATCGCTCTTCTGTGTGCTGCTATCGCAGTCGCTCAGTGCAACCCTGTCGTTCACGGACACAAAGCACTCGCTAGCACCGGAGTGAGTTCCAGGTCCCAGTCCCAAGATGGATACGGAAACTACGCTTTCGGTTATGACATCAAGGACGCTCTGGGTGCCACCAACTCC"
+
+original = "------------------------------------------------MFAKIALLCAAIAVAQCNPVVH--------------------GHKA----LASTGVSSRSQ-------SQD--GYGNYAFGYDIKDA---LGA-TNS------------------------------------------------------------------------------------------------------------"
+cut=[49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 138, 139, 142, 143, 144, 145, 146, 147, 148, 149]
+
+new = "FAKIALLCAAIAVAQCNLASTGVSSRSQYGNYAFGYDIKDALGTNS-----"
+
+CodonPos={}
+position=0
 codon=""
-codonTable = {"TTT":"F", "TTC":"F", "TTA":"L", "TTG":"L",
-    "TCT":"S", "TCC":"C", "TCA":"S", "TCG":"S",
-    "TAT":"Y", "TAC":"Y", "TAA":"*", "TAG":"*",
-    "TGT":"C", "TGC":"C", "TGA":"*", "TGG":"W",
-    "CTT":"L", "CTC":"L", "CTA":"L", "CTG":"L",
-    "CCT":"P", "CCC":"P", "CCA":"P", "CCG":"P",
-    "CAT":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
-    "CGT":"R", "CGC":"R", "CGA":"R", "CGG":"R",
-    "ATT":"I", "ATC":"I", "ATA":"I", "ATG":"M",
-    "ACT":"T", "ACC":"T", "ACA":"T", "ACG":"T",
-    "AAT":"N", "AAC":"N", "AAA":"K", "AAG":"K",
-    "AGT":"S", "AGC":"S", "AGA":"R", "AGG":"R",
-    "GTT":"V", "GTC":"V", "GTA":"V", "GTG":"V",
-    "GCT":"A", "GCC":"A", "GCA":"A", "GCG":"A",
-    "GAT":"D", "GAC":"D", "GAA":"E", "GAG":"E",
-    "GGT":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
-protein =""
-for i in nuc:
+number=1
+print len(cds)/3
+for i in cds:
+
     codon +=i
-    print i,position%3,codon
+    #print i,position%3,codon
     if position%3==2:
-        print codon
-        print codonTable[codon]
-        protein+=codonTable[codon]
+        #print codon
+        #print codonTable[codon]
+        CodonPos[number]=codon
+        number+=1
+        #protein+=codonTable[codon]
     position +=1
 
     if position%3==0:
         #print codon
         codon=""
-print protein
-print    "MNFSTIIILLIIGISYATGLSTNQGSTRSKKGALMEIERRCIGKEQECTDNKGGCCGDMKCLCYKNVMDENEKGCWCGSGTYYFIEKP*"
+print len(cut)
+print "****************"
+print CodonPos
+print "****************"
 
-original = "------------------MNFSTIIILLIIGISYATGLSTNQGSTRSKKGALMEI-ERRCIGKEQECTDNKGGCCG-----DMKCLCYKN-----VMDENEKGCWCGSGTYYFIE-KP"
-print len(original),original
-trimmed= "MNFSTIIILLIIGISYATGLSTNQGSTRSKKMEI-ERRCIGKEQECTDNKGGCCGDMKCLCYKNDENEKGCWCGSGTYY"
-
-#number of gaps in original
-lengthOriginal = len(original)
-gapOriginal = original.count('-')
-aaOriginal = lengthOriginal - gapOriginal
-
-lengthTrimm = len(trimmed)
-gapTrim = trimmed.count('-')
-aaTrim = lengthTrimm - gapTrim
-
-gapLost = gapOriginal - gapTrim
-aaLost = aaOriginal - aaTrim
-
-aaFreq = {}
-numberS=0
+aaPos = 1
+translated = ""
 for i in original:
-    if i != "-":
-        if i not in aaFreq:
-            aaFreq[i]=original.count(i)
-            numberS+=original.count(i)
-print aaFreq
-lostAndFound ={}
-for i in trimmed:
     if i !="-":
-        print aaFreq[i], trimmed.count(i)
-        if aaFreq[i] != trimmed.count(i) and i not in lostAndFound:
-            print "You lost",aaFreq[i]- trimmed.count(i),i
-            lostAndFound[i]=True
-            #print i
-        #print aaFreq[i]
-print aaOriginal
-print aaTrim
-print aaLost,len(lostAndFound)
-print numberS
+        translated+=CodonPos[aaPos]
+    else:
+        translated+="-"
+print translated
+column=0
+trimmed=""
+aaPos=1
+prot=""
+for i in original:
+    if column  in cut:
+        if i =="-":
+            trimmed+="---"
+            prot+=i
+        else:
+            trimmed+=CodonPos[aaPos]
+            prot+=i
+            aaPos+=1
+    column+=1
+print column
+print trimmed
+print prot
+print len(prot)
+print len(trimmed)
+print len(trimmed)/3
