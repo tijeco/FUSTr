c4c3cc3847f61c5312be12eb5d50c36c28cdfa1b
Jeff Cole <jeffcole@hygstl1.intra.ecu.edu>
2017-05-03 22:32:25 -0400

    moved colnum

--- HeaderPattern.py
+++ HeaderPattern.py
@@ -58,6 +58,9 @@ for ff in sequence_iterator:
     except:
         colNum = len(splitHeader)
         usableColumns = colNum
+    colNum = len(splitHeader)
+
+
     for i in range(len(splitHeader)):
         if i not in newDict:
             newDict[i] = {}
