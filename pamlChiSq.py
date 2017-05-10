295099128da1c55bb52280cff2301b38fb9e5de5
Jeff Cole <jeffcole@Jeffs-MacBook-Pro.local>
2017-05-10 07:02:30 -0400

    from web

--- pamlChiSq.py
+++ pamlChiSq.py
@@ -1,7 +1,7 @@
 import sys
 import os.path
 from Bio.Phylo.PAML.chi2 import cdf_chi2
-print(cdf_chi2(1, 6.2))
+print(cdf_chi2(1, 7.21))
 
 def getOptionValue(option):
     optionPos = [i for i, j in enumerate(sys.argv) if j == option][0]
