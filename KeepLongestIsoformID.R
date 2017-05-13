cd3e83d24b995deffe3205a2c8495115b0bfa46b
tijeco <coleti16@students.ecu.edu>
2017-05-13 03:01:19 -0400

    Delete KeepLongestIsoformID.R

--- KeepLongestIsoformID.R
+++
@@ -1,6 +0,0 @@
-args <- commandArgs(TRUE)
-transcripts <- read.table(args[1])
-
-aa <- transcripts[order(transcripts$V1, -abs(transcripts$V3) ), ]
-
-write.table( aa[ !duplicated(aa$V1), ] ,file=args[2],row.names=F,col.names=F,quote=F)
