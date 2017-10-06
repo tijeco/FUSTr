cf35bd232e8446b12525b3dd7fde48b82771d366
Jeff Cole <coleti16@students.ecu.edu>
2017-10-06 10:22:37 -0400

    configures docker container

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,1 +1,11 @@
-n
+FUSTR_dir=$(pwd)
+target=${1%/}
+
+cp -r $target $FUSTR_dir
+echo cp -r  $target $FUSTR_dir
+echo $FUSTR_dir
+# pwd
+work_dir=$FUSTR_dir/$(basename $target)
+echo $work_dir
+ls $work_dir
+echo docker build -t  fuster --build-arg package=$work_dir  $FUSTR_dir --no-cache
