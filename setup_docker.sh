d24e9a47bce5d4131d67accb1601c4bf5dd6ddcf
Jeff Cole <coleti16@students.ecu.edu>
2017-10-06 12:02:33 -0400

    just basename

--- setup_docker.sh
+++ setup_docker.sh
@@ -2,10 +2,10 @@ FUSTR_dir=$(pwd)
 target=${1%/}
 
 cp -r $target $FUSTR_dir
-echo cp -r  $target $FUSTR_dir
+echo cp -r  $target $FUSTR_dir/$(basename $target)
 echo $FUSTR_dir
 # pwd
-work_dir=$FUSTR_dir/$(basename $target)
+# work_dir=$FUSTR_dir/$(basename $target)
 echo $work_dir
 ls $work_dir
-echo docker build -t  fuster --build-arg package=$work_dir  $FUSTR_dir --no-cache
+docker build -t  fustr --build-arg package=$(basename $target)  $FUSTR_dir --no-cache
