7b7e43c6b8cb970f9169d16b0f14be8ebca99ac7
Jeff Cole <coleti16@students.ecu.edu>
2017-10-07 05:59:41 -0400

    better

--- Simulations/makeSeed.py
+++ Simulations/makeSeed.py
@@ -26,7 +26,7 @@ for i in range(int(sys.argv[1])):
     length = random.randint(50, 150)
     branchlength = random.randint(1,15)
     regime = random.choice(["pos","pur","con"])
-    filename = "seeds/seed"+regime+str(i)+"_len"+str(length)+"BL_"+str(branchlength)+regime+".fa"
+    filename = "seeds/seed"+regime+str(i)+"_len"+str(length)+"BL_"+str(branchlength)".fa"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, "w") as out:
         out.write(makeSeed(length))
