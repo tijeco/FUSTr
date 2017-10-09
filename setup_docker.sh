a7af707ebb55777a42a0d9f95c43e8d14be9f9df
Jeff Cole <coleti16@students.ecu.edu>
2017-10-08 20:13:52 -0400

    sets up docker container

--- setup_docker.sh
+++ setup_docker.sh
@@ -4,8 +4,7 @@ target=${1%/}
 cp -r $target $FUSTR_dir
 echo cp -r  $target $FUSTR_dir/$(basename $target)
 echo $FUSTR_dir
-# pwd
-# work_dir=$FUSTR_dir/$(basename $target)
+
 echo $work_dir
 ls $work_dir
 docker build -t  fustr --build-arg package=$(basename $target)  $FUSTR_dir --no-cache
