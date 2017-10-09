1360940e913845b7fda668e42285ce3af1a7ed81
Jeff Cole <coleti16@students.ecu.edu>
2017-10-09 08:14:44 -0400

    back to previous settings

--- Simulations/makeSeed.py
+++ Simulations/makeSeed.py
@@ -23,8 +23,10 @@ def makeSeed(numAA):
 # seedNum = int(sys.argv[1])
 
 for i in range(int(sys.argv[1])):
-    length = random.randint(350, 500)
-    branchlength = random.randint(1,15)
+    # length = random.randint(350, 500)
+    length = random.randint(50, 1000)
+    # branchlength = random.randint(1,15)
+    branchlength = random.randint(1,50)
     regime = random.choice(["pos","pur","con"])
     filename = "seeds/seed"+regime+str(i)+"_len"+str(length)+"BL_"+str(branchlength)+".fa"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
