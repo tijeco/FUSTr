6922d26865a1bbfe262c94482cab96fc3a424a35
Jeff Cole <coleti16@students.ecu.edu>
2019-10-24 15:01:20 -0400

    input and output flags

--- utils/dollo_extractNodeFams.py
+++ utils/dollo_extractNodeFams.py
@@ -1,4 +1,25 @@
 import sys
+import os.path
+from itertools import groupby
+
+def getOptionValue(option):
+    optionPos = [i for i, j in enumerate(sys.argv) if j == option][0]
+    optionValue = sys.argv[optionPos + 1]
+    return optionValue
+
+if "-i" in sys.argv:
+    working_dir = getOptionValue("-i").strip("/")
+else:
+    print("\nplease specify input file name using -i <file_name> \n")
+    sys.exit()
+
+if "-o" in sys.argv:
+    working_dir = getOptionValue("-o").strip("/")
+else:
+    print("\nplease specify output file name using -o <file_name> \n")
+    sys.exit()
+
+
 start = False
 start_dict = {}
 start_key = ""
@@ -28,4 +49,4 @@ for node in start_dict:
 #       print(len(start_dict[node].replace(" ",'')))
         for i in range(len(start_dict[node].replace(" ",''))):
                 if start_dict[node].replace(" ",'')[i] == "1":
-                        print(node,"FUSTrFam_"+str(i+1))
+                        out.write(str(node)+" FUSTrFam_"+str(i+1))
